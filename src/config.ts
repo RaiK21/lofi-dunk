bd910af6ca23ad5661de6e31f7638d3dddacbf83
RaiK <97009165+RaiK21@users.noreply.github.com>
2023-08-13 13:37:24 +0800

    add initial files

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+import { BootScene } from './scenes/boot-scene';
+import { GameScene } from './scenes/game-scene';
+import { MainMenuScene } from './scenes/main-menu-scene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Lofi Dunk',
+  url: 'https://github.com/digitsensitive/phaser3-typescript',
+  version: '1.0',
+  type: Phaser.AUTO,
+  scale: {
+    parent: 'game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 720,
+    height: 1280,
+  },
+  scene: [BootScene, MainMenuScene, GameScene],
+  input: {
+    keyboard: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 }
+    }
+  },
+  backgroundColor: '#98d687',
+  // render: { pixelArt: true, antialias: false }
+};
